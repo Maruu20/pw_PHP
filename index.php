8c69c3829450db7a6c26aa974cf73fea316e7b3f
Maruu20 <namikazeamuro@gmail.com>
2026-04-13 12:21:33 +0800

    commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$nama = "yudi";
+$umur = 100;
+$tinggi = 200;
+$menikah = true;
+$hobi = ["membaca"];
+
+echo "nama saya $nama, umur saya $umur, tinggi saya $tinggi cm, status saya $menikah, hobi saya $hobi[0]";
+?>
