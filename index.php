dcb1eebbfc647f3ff62e95fe3e11875bf624adcd
Maruu20 <namikazeamuro@gmail.com>
2026-04-16 08:39:56 +0800

    percaabangan dan operator

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ echo "nama saya $nama, umur saya $umur, tinggi saya $tinggi cm, status saya $men
 
 
 echo "<br><br>==============================<br><br>";
+
+
 //OPERATOR
 $nilai1 = 5;
 $nilai2 = 8;
@@ -20,5 +22,17 @@ $hasil = $nilai1 + $nilai2 - $nilai3 * $nilai4 /$nilai5;
 
 echo "hasil dari $nilai1 + $nilai2 * $nilai3 - $nilai4 /$nilai5";
 
-
+//Percabangan
+$nilai = 70;
+if($nilai >= 85){
+    echo "Grade A";
+} else if ($nilai >= 70) {
+    echo "Grade B";
+} else if ($nilai >= 50) {
+    echo "Grade C";
+} else if ($nilai >= 30) {
+    echo "Grade D";
+} else{
+    echo "Grade E";
+}
 ?>
