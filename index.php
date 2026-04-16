7d2c530c6a56ed0b4bb5800e9630267ac1c2f98d
Maruu20 <namikazeamuro@gmail.com>
2026-04-16 08:11:59 +0800

    versi 1

--- index.php
+++ index.php
@@ -6,4 +6,19 @@ $menikah = true;
 $hobi = ["membaca"];
 
 echo "nama saya $nama, umur saya $umur, tinggi saya $tinggi cm, status saya $menikah, hobi saya $hobi[0]";
+
+
+echo "<br><br>==============================<br><br>";
+//OPERATOR
+$nilai1 = 5;
+$nilai2 = 8;
+$nilai3 = 10;
+$nilai4 = 20;
+$nilai5 = 13;
+
+$hasil = $nilai1 + $nilai2 - $nilai3 * $nilai4 /$nilai5;
+
+echo "hasil dari $nilai1 + $nilai2 * $nilai3 - $nilai4 /$nilai5";
+
+
 ?>
